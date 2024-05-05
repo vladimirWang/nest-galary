308290e6d939cb91d8ef7b21d131d94c88b1aadc
wangxiaoyu006 <wangxiaoyu006@chinasofti.com>
2024-05-05 12:56:33 +0800

    配置数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'galary',
+      synchronize: true,
+      logging: true,
+      entities: [],
+      migrations: ['./src/migration/**.ts'],
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+        authPlugin: 'sha256_password',
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
