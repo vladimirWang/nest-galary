dcda63f30ef3a609fbc26d0c87e33c1b2c3a4d72
wangxiaoyu006 <wangxiaoyu006@chinasofti.com>
2024-05-06 23:11:03 +0800

    接口不操作数据库

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,16 +4,17 @@ import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class UserService {
-  @Inject(PrismaService)
-  private prisma: PrismaService;
+  // @Inject(PrismaService)
+  // private prisma: PrismaService;
 
   // Prisma.UserCreateInput
-  async create(data: any) {
-    return await this.prisma.user.create({
-      data,
-      select: {
-        id: true,
-      },
-    });
+  async create(data) {
+    return 'create user success';
+    // return await this.prisma.user.create({
+    //   data,
+    //   select: {
+    //     id: true,
+    //   },
+    // });
   }
 }
