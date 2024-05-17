b5ed6439ed9acb69ee272f6b5a5ef6e6b25fd58d
wangxiaoyu006 <wangxiaoyu006@chinasofti.com>
2024-05-17 08:56:33 +0800

    user模块crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'galary2',
+      synchronize: true,
+      logging: true,
+      entities: [User],
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+        authPlugins: 'sha256_password',
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
