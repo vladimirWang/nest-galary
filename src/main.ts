577f76edc826c4894b96e8c8eca1897d33b97eac
wangxiaoyu006 <wangxiaoyu006@chinasofti.com>
2024-05-18 22:31:14 +0800

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 配置swagger
+  const options = new DocumentBuilder()
+    .setTitle('blog')
+    .setDescription('my first nestjs project')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
