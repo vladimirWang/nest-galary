4e3d2b4d1698d818d0fd532b3f1ff830dbcdfc76
wangxiaoyu006 <wangxiaoyu006@chinasofti.com>
2024-05-24 07:36:01 +0800

    sql

---
+++ sql/createUserTbl.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
